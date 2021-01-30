66c38daf34470686c378f1d84aff2d611b24c074
Yuta OHWATARI <yuta_ohwatari@voyagegroup.com>
2021-01-31 02:37:11 +0900

    cb prefix

--- src/cpu.rs
+++ src/cpu.rs
@@ -4,6 +4,7 @@ use crate::cpu::register::Flags;
 use crate::cpu::register::Register;
 use crate::cpu::register::Register16::{AF, BC, DE, HL};
 use crate::mmu::MMU;
+use crate::open_rom_file;
 
 #[derive(Debug)]
 pub struct CPU {
@@ -41,7 +42,16 @@ impl CPU {
             0x01 | 0x11 | 0x21 | 0x31 => self.ld_n_nn(opcode),
             0xaf | 0xa8 | 0xa9 | 0xaa | 0xab | 0xac | 0xad | 0xae | 0xee => self.xor_n(opcode),
             0x32 => self.ldd_hl_a(),
+            0xcb => self.prefix(),
             _ => unimplemented!("unknown opcode: 0x{:02x}\ncpu: {:?}", opcode, self),
+        }
+    }
+
+    fn prefix(&mut self) -> u32 {
+        let opcode = self.fetch_byte();
+        match opcode {
+            0x40..=0x7f => self.bit_b_r(opcode),
+            _ => unimplemented!("unknown cb opcode: 0x{:02x}\ncput: {:?}", opcode, self),
         }
     }
 
@@ -67,45 +77,15 @@ impl CPU {
 
     fn xor_n(&mut self, opcode: u8) -> u32 {
         match opcode {
-            0xaf => {
-                let n = self.register.a;
+            0xa8..=0xaf => {
+                let r8 = opcode & 0b_0000_0111;
+                let n = self.read_r8(r8);
                 self._xor_n(n);
-                4
-            }
-            0xa8 => {
-                let n = self.register.b;
-                self._xor_n(n);
-                4
-            }
-            0xa9 => {
-                let n = self.register.c;
-                self._xor_n(n);
-                4
-            }
-            0xaa => {
-                let n = self.register.d;
-                self._xor_n(n);
-                4
-            }
-            0xab => {
-                let n = self.register.e;
-                self._xor_n(n);
-                4
-            }
-            0xac => {
-                let n = self.register.h;
-                self._xor_n(n);
-                4
-            }
-            0xad => {
-                let n = self.register.l;
-                self._xor_n(n);
-                4
-            }
-            0xae => {
-                let n = self.mmu.read_byte(self.register.read_word(HL));
-                self._xor_n(n);
-                8
+
+                match r8 {
+                    6 => 8,
+                    _ => 4,
+                }
             }
             0xee => {
                 let n = self.fetch_byte();
@@ -121,5 +101,38 @@ impl CPU {
         self.mmu.write_byte(hl, self.register.a);
         self.register.write_word(HL, hl.wrapping_sub(1));
         8
+    }
+
+    fn bit_b_r(&mut self, opcode: u8) -> u32 {
+        let b = (opcode & 0b_0011_1000) >> 3;
+        let r8 = opcode & 0b_0000_0111;
+        let reg = self.read_r8(r8);
+
+        let result = reg & (1 << b);
+        self.register.set_flag(Flags::Z, result == 0);
+        self.register.set_flag(Flags::N, false);
+        self.register.set_flag(Flags::H, true);
+
+        match r8 {
+            6 => 16,
+            _ => 8,
+        }
+    }
+
+    fn read_r8(&self, r8: u8) -> u8 {
+        match r8 {
+            0 => self.register.b,
+            1 => self.register.c,
+            2 => self.register.d,
+            3 => self.register.e,
+            4 => self.register.h,
+            5 => self.register.l,
+            6 => {
+                let hl = self.register.read_word(HL);
+                self.mmu.read_byte(hl)
+            }
+            7 => self.register.a,
+            _ => unreachable!("invalid operand index: {}", r8),
+        }
     }
 }
